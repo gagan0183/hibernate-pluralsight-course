ab9d3adab578afbbc36ffbadfa7101959c9a1952
Gagan <g.deepsingh1@gmail.com>
2017-07-11 11:29:40 +0530

    create map collections

--- src/com/simpleprogram/Program.java
+++ src/com/simpleprogram/Program.java
@@ -1,6 +1,7 @@
 package com.simpleprogram;
 
 import java.util.Date;
+import java.util.Map.Entry;
 
 import org.hibernate.Session;
 
@@ -10,25 +11,30 @@ public class Program {
 		System.out.println("Hello");
 		Session session = HibernateUtilities.getSessionFactory().openSession();
 		session.beginTransaction();
-		
+
 		User user = new User();
 		user.setName("user");
 		user.getProteinData().setGoal(90);
-		user.getUserHistories().add(new UserHistory(new Date(), "Set the goal to 90"));
+		user.getUserHistories().put("1", new UserHistory(new Date(), "Set the goal to 90"));
 		session.save(user);
 
 		session.getTransaction().commit();
-		
+
 		session.beginTransaction();
-		
+
 		User loaduser = (User) session.get(User.class, 1);
 		System.out.println(loaduser.getName());
-		
+
+		for (Entry<String, UserHistory> entry : loaduser.getUserHistories().entrySet()) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue().getEntryDate() + " " + entry.getValue().getEntry());
+		}
+
 		loaduser.getProteinData().setTotal(loaduser.getProteinData().getTotal() + 90);
-		loaduser.getUserHistories().add(new UserHistory(new Date(), "Set the total to 90"));
+		loaduser.getUserHistories().put("2", new UserHistory(new Date(), "Set the total to 90"));
 
 		session.getTransaction().commit();
-		
+
 		session.close();
 		HibernateUtilities.getSessionFactory().close();
 	}
--- src/com/simpleprogram/User.java
+++ src/com/simpleprogram/User.java
@@ -1,35 +1,43 @@
 package com.simpleprogram;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
 	private int id;
 	private String name;
 	private ProteinData proteinData = new ProteinData();
-	private Set<UserHistory> userHistories = new HashSet<>();
+	private Map<String, UserHistory> userHistories = new HashMap<String, UserHistory>();
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public ProteinData getProteinData() {
 		return proteinData;
 	}
+
 	public void setProteinData(ProteinData proteinData) {
 		this.proteinData = proteinData;
 	}
-	public Set<UserHistory> getUserHistories() {
+
+	public Map<String, UserHistory> getUserHistories() {
 		return userHistories;
 	}
-	public void setUserHistories(Set<UserHistory> userHistories) {
+
+	public void setUserHistories(Map<String, UserHistory> userHistories) {
 		this.userHistories = userHistories;
 	}
 }
